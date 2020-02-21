5622c8e9ebe863d0892a9adbbec749a08ab8733c
Joordep <joordep.m@gmail.com>
2020-02-20 21:25:03 -0300

    Update App.vue
    Adjustments

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
 
           <md-button
             class="md-icon-button md-raised md-primary"
-            @click="createNewRecord(inventoryItem)"
+            @click="createInventoryItem(inventoryItem)"
           >
             <md-icon>save</md-icon>
           </md-button>
@@ -48,7 +48,7 @@
         <md-table-cell md-label="Price" md-sort-by="price__c">{{item.price__c}}</md-table-cell>
         <md-table-cell md-label="Quantity" md-sort-by="Quantity__c">{{item.Quantity__c}}</md-table-cell>
         <md-table-cell md-label="Edit">  
-          <md-button class="md-icon-button md-accent" @click="editRecord(item)">
+          <md-button class="md-icon-button md-accent" @click="editInventoryItem(item)">
             <md-icon>edit</md-icon>
           </md-button>
         </md-table-cell> 
@@ -89,7 +89,7 @@ export default {
   },
   methods: {
     list() {
-      axios({ method: "GET", url: "http://localhost:8080/getAll" }).then(
+      axios({ method: "GET", url: "http://localhost:8080/getInventoryList" }).then(
         response => (this.inventory = response.data.records)
       );
     },
@@ -97,46 +97,46 @@ export default {
       if (confirm("Are you sure ?")) {
         axios({
           method: "DELETE",
-          url: "http://localhost:8080/deleteById",
+          url: "http://localhost:8080/deleteInventoryItemById",
           params: { id: id }
         }).then(response => (console.log(response), this.list()));
       }
     },
-    createNewRecord() {
+    createInventoryItem() {
       if(!this.inventoryItem.Id){
         axios
-          .post("http://localhost:8080/createNew", this.inventoryItem)
+          .post("http://localhost:8080/createInventoryItem", this.inventoryItem)
           .then(
             response => (
               console.log(response),
               this.cleanFields(),
               this.list(),
-              alert("Saved")
+              alert("Saved!")
             )
           );
       }else{
-        this.updateRecord()
+        this.updateInventoryItem()
       }
     },
     cleanFields() {
       this.inventoryItem = {};
     },
-    editRecord(item){      
+    editInventoryItem(item){      
       this.inventoryItem.Id = item.Id
       this.inventoryItem.Name = item.Name
       this.inventoryItem.price__c = item.price__c
       this.inventoryItem.description__c = item.description__c
       this.inventoryItem.Quantity__c = item.Quantity__c
     },
-    updateRecord(){
+    updateInventoryItem(){
       axios
-        .patch("http://localhost:8080/updateItem", this.inventoryItem)
+        .patch("http://localhost:8080/updateInventoryItem", this.inventoryItem)
         .then(
           response => (
             console.log(response),
             this.cleanFields(),
             this.list(),
-            alert("Saved")
+            alert("Item updated!")
           )
         );
     }
